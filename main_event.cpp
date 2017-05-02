d2466c26dda6376d8df198e41176a78d9fb4fe21
MP <maxmaxzmayank@gmail.com>
2017-05-02 18:05:18 +0530

    Easy part's over!! Worst yet to come......
    Final translation
    Synchronization
    Menu driven (if time left over)
    Final Working Model
    yet to be taken care of!! I hate myself!!

--- main_event.cpp
+++ main_event.cpp
@@ -117,7 +117,7 @@ void cell_membrane(void)
         glPopMatrix();
 }
 
-void cells(void) //totally static
+void cells(void) //total static
 {
         int j = 0, k = 0;
         //right upper portion
@@ -300,7 +300,7 @@ void concentration_indicator(void)
         int k = 0, l = 0;
         for(int i=1; i <= 20; i += 1)
         {
-                glColor3ub(230 - k, 0 + k, 0); // transition of color indicating increase in electronic gradient potential
+                glColor3ub(230 - k, 0 + k, 0); // transparency indicating increase in electronic gradient potential
                 glBegin(GL_QUADS);
                         glVertex2f(180, -45+l);
                         glVertex2f(183, -45+l);
@@ -326,43 +326,167 @@ void concentration_indicator(void)
         glPopMatrix();
 }
 
-float goxp = 0, goxn = 0;
-void glucose_out(void)
+float nalx = 0, narx = 0;
+void sodium(void)
 {
-        glPushMatrix();
-        if(goxp <= 20 && !paused)
-        {
-                goxp += 0.0053;
-                glTranslatef(goxp, 0, 0);
-                goxn = goxp;
-        }
-        else if(goxn >= 0 && !paused)
-        {
-                goxn -= 0.0053;
-                glTranslatef(goxn, 0, 0);
-        }
-        glColor3f(0.5, 1, 0.4);
         int k = 0;
-        for(int i = 1; i <= 10; i += 1)
+        glPushMatrix();
+        if (narx <= 20 && !paused)
+        {
+                narx += 0.0053;
+                glTranslatef(narx, 0, 0);
+                nalx = narx;
+        }
+        else if (nalx > 0 && !paused)
+        {
+                nalx -= 0.0053;
+                glTranslatef(nalx, 0, 0);
+        }
+        glColor3f(0.5, 0, 0.2);
+        for(int i = 1; i <= 9; i += 1)
         {
                 glBegin(GL_POLYGON);
-                        glVertex2f(-110+ k, 50);
-                        glVertex2f(-115 + k, 45);
-                        glVertex2f(-110 + k, 40);
-                        glVertex2f(-105 + k, 40);
-                        glVertex2f(-100 + k, 45);
-                        glVertex2f(-105 + k, 50);
+                        glVertex2d(-100 + k, -45);
+                        glVertex2d(-105 + k, -50);
+                        glVertex2d(-100 + k, -55);
+                        glVertex2d(-95 + k, -50);
                 glEnd();
                 k += 25;
         }
         glPopMatrix();
 }
 
-/*float glulx = 0, glurx = 0, gluy = 0; //glucose movement
-void glucose_out_in(void)
+float atplx = 0, atprx = 0;
+void ATP(void)
 {
-                glColor3f()
-}*/
+        int k = 0;
+        glPushMatrix();
+        if (atplx >= -20 && !paused)
+        {
+                atplx -= 0.0053;
+                glTranslatef(atplx, 0, 0);
+                atprx = atplx;
+        }
+        else if(atprx <= 0 && !paused)
+        {
+                atprx += 0.0053;
+                glTranslatef(atprx, 0, 0);
+        }
+        for(int i = 0; i <= 5; i += 1)
+        {
+                glColor3f(0, 1, 1);
+                glBegin(GL_POLYGON);
+                        glVertex2d(-120 + k, -90);
+                        glVertex2d(-110 + k, -90);
+                        glVertex2d(-115 + k, -80);
+                glEnd();
+                glColor3f(1, 1, 0);
+                glBegin(GL_POLYGON);
+                        glVertex2d(-120 + k, -90);
+                        glVertex2d(-123 + k, -95);
+                        glVertex2d(-117 + k, -95);
+                glEnd();
+                glBegin(GL_POLYGON);
+                        glVertex2d(-110 + k, -90);
+                        glVertex2d(-113 + k, -95);
+                        glVertex2d(-107 + k, -95);
+                glEnd();
+                glBegin(GL_POLYGON);
+                        glVertex2d(-120 + k, -90);
+                        glVertex2d(-123 + k, -95);
+                        glVertex2d(-117 + k, -95);
+                glEnd();
+                glBegin(GL_POLYGON);
+                        glVertex2d(-115 + k, -80);
+                        glVertex2d(-112 + k, -75);
+                        glVertex2d(-118 + k, -75);
+                glEnd();
+                k += 45;
+        }
+        glPopMatrix();
+}
+
+float klx = 0, krx = 0;
+void potassium(void)
+{
+        int k = 0;
+        glPushMatrix();
+        if (krx <= 20 && !paused)
+        {
+                krx += 0.0053;
+                glTranslatef(krx, 0, 0);
+                klx = krx;
+        }
+        else if (klx > 0 && !paused)
+        {
+                klx -= 0.0053;
+                glTranslatef(klx, 0, 0);
+        }
+        glColor3f(0.8, 0.3, 0.5);
+        for(int i = 1; i <= 6; i += 1)
+        {
+                glBegin(GL_POLYGON);
+                        glVertex2d(-90 + k, 35);
+                        glVertex2d(-97 + k, 42);
+                        glVertex2d(-90 + k, 49);
+                        glVertex2d(-83 + k, 42);
+                glEnd();
+                k += 25;
+        }
+        glPopMatrix();
+}
+
+float goxp = 0, goxn = 0;
+void glucose_out(void)
+{
+        glPushMatrix();
+        if(goxn >= -20 && !paused)
+        {
+                goxn -= 0.0053;
+                glTranslatef(goxn, 0, 0);
+                goxp = goxn;
+        }
+        else if(goxp <= 0 && !paused)
+        {
+                goxp += 0.0053;
+                glTranslatef(goxp, 0, 0);
+        }
+        glColor3f(0.5, 1, 0.4);
+        int k = 0;
+        for(int i = 1; i <= 5; i += 1)
+        {
+                glBegin(GL_POLYGON);
+                        glVertex2f(-110 + k, 90);
+                        glVertex2f(-115 + k, 85);
+                        glVertex2f(-110 + k, 80);
+                        glVertex2f(-105 + k, 80);
+                        glVertex2f(-100 + k, 85);
+                        glVertex2f(-105 + k, 90);
+                glEnd();
+                k += 50;
+        }
+        glColor3f(1, 1, 0.2);
+        k = 0;
+        for (int i = 1; i <= 5; i +=1)
+        {
+                glBegin(GL_POLYGON);
+                        glVertex2f(-108 + k, 75);
+                        glVertex2f(-113 + k, 70);
+                        glVertex2f(-113 + k, 65);
+                        glVertex2f(-108 + k, 60);
+                        glVertex2f(-103 + k, 65);
+                        glVertex2f(-103 + k, 70);
+                glEnd();
+                k += 50;
+        }
+        glPopMatrix();
+}
+
+//float glulx = 0, glurx = 0, gluy = 0; //glucose movement 3 Right : 2 Left
+//void glucose_out_in(void)
+//{
+//                glColor3f()
+//}
 
 // active transport initiation for glucose transportation when potential builds up; i.e. channel protein opens-up
 float cplxn = 0, cplxp = 0;
@@ -427,19 +551,19 @@ void channel_protein(void)
 void display(void)
 {
         glClear(GL_COLOR_BUFFER_BIT );//clear the window with current clearing color, i.e. removes the last drawing from the window
-//     first_page();
-        cell_membrane(); //partially done, i.e. opening
+        //     first_page();
+        // rotation yet to be applied
+        cell_membrane(); //partially done, i.e. opening and closing. waiting yet to be deployed
         cells(); //done
         concentration_indicator(); //done
-        // sodium();
-        //potassium();
-        //ATP();
+        sodium(); // modeling done, translation left
+        ATP(); // partially done. transition to ADP left over
+        potassium(); // modelling done, translation left
         glucose_out(); //done
         //glucose_out_in();
-        channel_protein(); //partially done, i.e. opening
-        //glFlush(); //causes any un-executed commands to be executed
+        channel_protein(); //partially done, i.e. opening and closing. waiting yet to be deployed
         glutSwapBuffers(); //for animation; glFlush() not required as it implicitly applies before rendering
-        glutPostRedisplay(); // iteration over rendering to show the movable parts
+        glutPostRedisplay();// iteration over rendering to show the movable parts
 }
 
 void reshape(GLsizei w, GLsizei h)
@@ -497,7 +621,7 @@ int main(int argc, char **argv)
         glutInitWindowSize(1000, 480);
         glutInitWindowPosition(0,0);
 //        init();
-        glutCreateWindow("K+/Na+ pump"); // creates window on the screen
+        glutCreateWindow("Na+/K+ pump"); // creates window on the screen
         glClearColor(0, 0, 0 , 0); //initialization before rendering
         glutDisplayFunc(display); //called whenever window needs to be drawn
         glutKeyboardFunc(handleKeypress);
